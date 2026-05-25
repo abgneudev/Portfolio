6fb40057a0dc954c5c29baeb82408f6c26eda640
Abhinav Gupta <gupta.abhinav@northeastern.edu>
2026-05-24 20:48:28 -0400

    updated resume

--- src/components/Hero/components/HeroHeader.tsx
+++ src/components/Hero/components/HeroHeader.tsx
@@ -198,7 +198,7 @@ export const HeroHeader = memo(function HeroHeader({
               className={styles.ctaSecondary}
               onClick={() => {
                 analytics.trackResume();
-                window.open('https://drive.google.com/file/d/1eFT70UT4UDwIG7v7-W6sS2Ywy2ZX-Q33/view?usp=sharing', '_blank');
+                window.open('https://drive.google.com/file/d/1iitbU5CBoTVMvmEUIY61v80JV-yhUrzC/view?usp=sharing', '_blank');
               }}
             >
               {isMobile ? 'CV' : <span className={styles.ctaWithIcon}>Resume <DownloadIcon /></span>}
